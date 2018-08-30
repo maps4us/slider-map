00a413382d050ddf82ae1acf5f5e18707ecde23a
Mike Menaker <michael.menaker@gmail.com>
2018-08-30 15:22:32 -0700

    feat: ui updates (#24)
    * feat: UI updates
    
    * Adding button that will zoom out map to default level
    * Adding tool tip for slider tabs so users know what year they represent
    * Switching connect elements to company colors
    
    * Formatting, upping version

--- src/map.js
+++ src/map.js
@@ -26,6 +26,8 @@ export function createMap(google, mapControlId, icon) {
         }
     });
 
+    createResetZoomControl();
+
     if (icon && icon.length) {
         _icon = icon;
     }
@@ -91,4 +93,37 @@ function openInfoWindow(content, gmarker) {
         content: content
     });
     _infoWindow.open(_map, gmarker);
+}
+
+function createResetZoomControl() {
+    let controlDiv = document.createElement('div');
+
+    // https://developers.google.com/maps/documentation/javascript/controls
+    // Set CSS for the control border.
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginLeft = '10px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Reset Zoom';
+    controlDiv.appendChild(controlUI);
+
+    // Set CSS for the control interior.
+    let controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = 'Reset Zoom';
+    controlUI.appendChild(controlText);
+
+    controlUI.addEventListener('click', () => _map.setZoom(3));
+
+    controlDiv.index = 1;
+    _map.controls[_google.maps.ControlPosition.LEFT_BOTTOM].push(controlDiv);
 }
