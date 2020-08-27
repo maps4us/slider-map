b1435502d616c37dd3376af26153862a528ea882
Mike Menaker <Michael.Menaker@cerence.com>
2020-08-26 21:53:09 -0700

    update test map id

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 // @ts-ignore
 // eslint-disable-next-line no-undef
-const map = new TimeLineMap('undefined', 'mapid', 'controlid');
+const map = new TimeLineMap('testmap', 'mapid', 'controlid');
 map.create();
